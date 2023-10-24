65a3bc860f37872d4f2427a90a81b0b75af29c22
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-10-24 22:52:28 +0700

    Update twrp_RMX3085.mk

--- twrp_RMX3085.mk
+++ twrp_RMX3085.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_DEVICE := nashc
+PRODUCT_DEVICE := RMX3085
 PRODUCT_NAME := twrp_RMX3085
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3085
